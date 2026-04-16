4dc4754f170238b8b919277a7f6017a8b98204ed
AdahJerrie <adahjerry51@gmail.com>
2026-04-16 16:51:29 +0100

    thinketoy added and banner command from os added

--- main.go
+++ main.go
@@ -8,39 +8,31 @@ import (
 )
 
 func main() {
-	if len(os.Args) >= 2 {
-
-	}
-	bannerfile := "standard.txt"
-
-	text := os.Args[1]
-	if len(os.Args) < 2 {
-		fmt.Println("Invalid Arg length")
-		return
+	bannerfile := "standard"
+	if len(os.Args) > 2 {
+		bannerfile = os.Args[2]
 	}
 
-	file, err := os.ReadFile(bannerfile)
+	banner := fmt.Sprintf("%s.txt", bannerfile)
+
+	data, err := os.ReadFile(banner)
 	if err != nil {
 		log.Fatal(err)
 	}
-	content := string(file)
 
-	lines := strings.Split(content, "\n") // split banner files by newlines
-	textlines := strings.Split(text, "\\n")
-	var result string
+	input := os.Args[1]
+	char := string(data)
+	slicedata := strings.Split(char, "\n")
+	sliceinput := strings.Split(input, "\\n")
+	result := ""
 
-	for _, val := range textlines {
-		//I want to print characters side by side, so I must create the row loop to capture the ASCII height
-		for row := 0; row < 8; row++ {
-			//loop through each char of the input string (to get the rune) --> horizontal control
-			for _, char := range val {
-				start := (int(char) - 32) * 9 // calculate start for each character
-				//lookup the banner file(splitfile) and print for each row of the char.
-				result += lines[start+row]
+	for _, line := range sliceinput {
+		for row := 0; row < 8; row++ { //I want to print characters side by side, so I must create the row loop to capture the ASCII height
+			for _, runes := range line {
+				result += slicedata[((int(runes)-32)*9+2)+row] //lookup the banner file(slicedata) and print for each row of the char.
 			}
 			result += "\n" // after finishing one row, add a new line so it prints the next row on a new line
 		}
 	}
-
-	fmt.Print(result)
+	fmt.Println(result)
 }
